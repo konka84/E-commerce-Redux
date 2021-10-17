1693fb16e5f68619103b2c89501e9f7d750ebfb0
konka <1784konka@gmail.com>
2021-10-17 17:42:30 +0600

    remove

--- src/components/cards/ProductCardSidebar.js
+++ src/components/cards/ProductCardSidebar.js
@@ -24,9 +24,8 @@ export default function ProductCardSidebar({ product: { id, title, image, price,
                 </div>
             </div>
             <div className="flex w-full py-1">
-                <button className="bg-purple-400 fa-fa-trash transition transform hover:scale-110 w-1/4 py-0 px-2 font-semibold rounded-md" onClick={() =>{dispatch(remove(id))
-                    
-                }} ><i className="fa fa-trash"></i></button>
+                <button className="bg-purple-400 fa-fa-trash transition transform hover:scale-110 w-1/4 py-0 px-2 font-semibold rounded-md" onClick={() =>dispatch(remove(id))} >
+                    <i className="fa fa-trash"></i></button>
                 <div className="flex space-x-3 justify-around flex-grow px-8 space-x-4">
                     <h3 className="flex-grow bg-purple-300 px-3 py-0 text-center rounded-lg font-bold text-lg" >{cartProducts[id].count}</h3>
                     <button onClick={() =>  dispatch(decrease(id))} className="bg-purple-400 transition transform hover:scale-110 px-2 py-0 rounded-full" ><i className="fa fa-minus font-bold text-xl"></i></button>
